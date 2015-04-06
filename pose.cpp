56c880a0fa402bd978f3bec4e5ba3a812e7adefc
otter11 <marco.trabattoni@outlook.com>
2015-04-06 15:30:31 +0200

    pose

---
+++ pose.cpp
@@ -0,0 +1,62 @@
+#include "pose.h"
+#include <cmath>
+
+namespace Data{
+
+Pose::Pose()
+{
+  aX(0);
+  aY(0);
+  aTheta(0);
+  r(0);
+  phi(0);
+}
+
+Pose::Pose(int aX, int aY, double aTheta, int r, int phi)
+{
+  aX(aX);
+  aY(aY);
+  aTheta(aTheta);
+  r(r);
+  phi(phi);
+}
+
+Pose::~Pose()
+{
+  
+}
+
+double Pose::getDistance(const Pose& pose)
+{
+  return std::sqrt((aX - pose.aX)*(aX - pose.aX) + (aY - pose.aY)*(aY - pose.aY));
+}
+
+int Pose::getX()
+{
+  return aX;
+}
+
+int Pose::getY()
+{
+  return aY;
+}
+
+double Pose::getTheta()
+{
+  return aTheta;
+}
+
+int Pose::getR()
+{
+  return r;
+}
+
+double Pose::getPhi()
+{
+  return phi;
+}
+
+
+
+
+}
---
+++ pose.h
@@ -0,0 +1,32 @@
+#ifndef POSE_H
+#define POSE_H
+
+namespace Data{
+
+  
+class Pose
+{
+public:
+  Pose();
+  Pose(int aX, int aY, double aTheta, int r, double phi);
+  virtual ~Pose();
+  
+  double getDistance(const Pose &pose); 
+  
+  int getX();
+  int getY();
+  double getTheta();
+  int getR();
+  double getPhi();
+  
+  
+private:
+  int aX, aY;		// x and y coordinates of the cell
+  double aTheta;	// orientation theta of th robot
+  int r;		// radius of the sensing operation 
+  double phi;		// central angle of the sensing operation
+  
+  
+};
+ 
+}
