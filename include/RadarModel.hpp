06ed13f9056f931e9b462bddc0385d4a6951d5a7
mfernandezcarmona@lincoln.ac.uk <mfernandezcarmona@lincoln.ac.uk>
2019-12-17 13:30:20 +0100

    New model with "addMeasurement" finally working. It should be straightforward to add it into mcdm models. For a detailled use case, check test_log_model.cpp

--- include/RadarModel.hpp
+++ include/RadarModel.hpp
@@ -1,1 +1,280 @@
-/home/manolofc/workspace/RFID/src/RFID/newModel/include/RadarModel.hpp
+#ifndef RADARMODEL_H
+#define RADARMODEL_H
+
+
+#include <grid_map_core/GridMap.hpp>
+#include "Eigen/Eigen"  // AFTER GRIDMAP!
+
+
+
+#include <Eigen/Core>
+#include <unsupported/Eigen/Splines>
+
+
+#include "grid_map_core/iterators/GridMapIterator.hpp"
+#include <iostream>
+#include <string>
+
+// Math
+#include <math.h>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#include <iomanip>
+
+
+#include "grid_map_cv/grid_map_cv.hpp"
+
+// OpenCV
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace grid_map;
+
+// constants ..................................................................
+// We mostly use UPM frog 3D.
+const double TAG_LOSSES = -4.8;
+
+// 20 * log10 ( c / (4*pi) )
+const double LOSS_CONSTANT = 147.55;
+// 4*pi/c
+const double PHASE_CONSTANT = 4.192e-8;
+
+// This comes from the manufacturer. Azimut
+// gain list entries start at -180 degrees to 180 in steps of 15.
+const double ANTENNA_LOSSES_LIST [25] = {  -22.6, -25.2, -25, -20.2, -17.6, -15.6, -14, -11.2, -7.8, -5.2, -2.4, -0.6, 0, -0.6, -2.4, -5.2, -8.8, -12.2, -16.4, -19.2, -20.8, -24.4, -28.2, -24, -22.6};
+const double ANTENNA_ANGLES_LIST [25] = {-180.0, -165.0, -150.0, -135.0, -120.0, -105.0, -90.0, -75.0, -60.0, -45.0, -30.0, -15.0, 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 90.0, 105.0, 120.0, 135.0, 150.0, 165.0, 180.0};
+
+
+
+
+// M6e RFID reader Specs
+// Minimum required power to identify a tag. Also depends on other factors, but
+// this value is a guide
+ const double SENSITIVITY = -115; // dB
+
+// Max transmitted power may be limited by the Region regulations
+// see Power Requirements  in M6e Hardware Guide
+ const double MIN_TX_POWER = -25; // dB
+ const double MAX_TX_POWER = 0; // dB
+ const double STEP_TX_POWER = 0.5; // dB
+
+// These freqs ARE limited depending on the region regulations
+// see Regional Frequency Quantization in M6e Hardware Guide
+const double STEP_FREQ = 25e3; // Hertzs
+const double MIN_FREQ_A= 865e6; //Hertzs
+const double MAX_FREQ_A= 869e6; //Hertzs
+const double MIN_FREQ_B= 902e6; //Hertzs
+const double MAX_FREQ_B= 928e6; //Hertzs
+
+// most likely we will use EU or NA regions....
+const double MIN_FREQ_EU= 865.6e6; //Hertzs
+const double MAX_FREQ_EU= 867.6e6; //Hertzs
+const double STEP_FREQ_EU = 100e3; // Hertzs
+
+const double MIN_FREQ_NA= 902e6; //Hertzs
+const double MAX_FREQ_NA= 928e6; //Hertzs
+const double STEP_FREQ_NA = 250e3; // Hertzs
+
+//  ..................................................................
+
+class SplineFunction {
+      public:
+        SplineFunction();
+        SplineFunction(Eigen::VectorXd const &x_vec, Eigen::VectorXd const &y_vec);
+
+        double interpRad(double x) const;
+        double interpDeg(double x) const;
+
+      private:        
+        double scaled_value(double x) const;
+
+        Eigen::RowVectorXd scaled_values(Eigen::VectorXd const &x_vec) const;
+
+        double x_min;
+        double x_max;
+        double y_min;
+        double y_max;
+
+        // Spline of one-dimensional "points."
+        Eigen::Spline<double, 1> spline_;
+};
+
+//////////////////////////
+
+
+class RadarModel
+  {
+    double _sizeXaa; //  x axis size in active area (m.). Related with number of rows
+    double _sizeYaa; //  y axis size in active area (m.). Related with number of cols
+
+    int _Ncol; // number of rows of reference and rfid belief maps (cells)
+    int _Nrow; // number of cols of reference and rfid belief maps (cells)
+    
+    double _resolution; // Active areas, reference, and belief maps resolution (m./cell)
+    double _txtPower; // transmitted power by RFID reader (dB)
+
+    // we only model here gaussian noise: flat fadding.
+    double _sigma_power;  // noise factor
+    double _sigma_phase;  // noise factor
+    
+    GridMap _active_area_maps;  // active areas. one per phase and power at each frequency.
+    GridMap _rfid_belief_maps;  // Prob. beliefs. One layer per tag. Also, one layer with reference map, mostly for tag layout representation.
+    //GridMap _ref_map;  
+    
+    std::vector<std::pair<double,double>> _tags_coords; // tag locations in reference map coords (m.)
+    int _numTags;  // rfid tags to consider
+    
+    std::vector<double> _freqs; // transmission frequencies (Hz.)
+    SplineFunction _antenna_gains;  // model for antenna power gain depending on the angle (dB.)
+
+  public:
+
+/**
+ * @brief Construct a new RadarModel object. 
+ *        It is based upon the link budget equation. See D. M. Dobkin, “The RF in RFID: Passive UHF RFID in Practice”, Elsevier, 2007 
+ * 
+ * @param nx X axis Size (m.)
+ * @param ny Y axis Size (m.)
+ * @param resolution  internal grid resolution (m./cell)
+ * @param sigma Noise factor standard deviation 
+ * @param txtPower Transmited Power (dB)
+ * @param freqs Freqs to be considered in model (Hz.)
+ * TODO We will use a fixed antenna model, Gaussian noise model and will assume tags are isotropic. This may need to be revisited ...
+ */
+
+
+
+
+
+    RadarModel(const double nx, const double ny,  const double resolution, const double sigma_power, const double sigma_phase, const double txtPower, const std::vector<double> freqs, const std::vector<std::pair<double,double>> tags_coords, const std::string imageFileURI ) ;
+    void PrintMap( std::string savePath);
+    void initRefMap(const std::string imageURI);
+    void getImage(std::string layerName, std::string fileURI);
+
+    /**
+     * @brief Get layer name corresponding to given frequency (Hz.)
+     * 
+     * @param freq_i 
+     * @return std::string 
+     */
+    std::string getLayerName(double freq_i);
+  /////////////////////
+
+
+/**
+ * returns a range vector FROM start TO stop (included) in step increments
+ * @param  start min Value in vector
+ * @param  stop  max Value in vector
+ * @param  step  increment
+ * @return       range vector
+ */
+std::vector<double> range(double start, double stop, double step);
+
+/**
+ * returns MT_242025NRHK antenna losses in a plane
+ * @param  angleRad spheric coordinate within the plane (radians)
+ * @return          dB losses
+ */
+double antennaPlaneLoss(double angleRad );
+
+/**
+ * my Sign function...
+ * @param  x
+ * @return   sign of x
+ */
+float sign(float x);
+
+/**
+ * Returns Spherical coordinates
+ * @param x          cartesian coordinate x (m.)
+ * @param y          cartesian coordinate y (m.)
+ * @param r          spheric coordinate r (m.)
+ * @param phi        relative azimut [0,2pi) (angle between XY projection and X) (radians)
+ */
+void getSphericCoords(double x, double y, double& r, double& phi);
+
+/**
+ * Get received power from an OMNIDIRECTIONAL tag,
+ * given its relative position to antenna.
+ * We assume antenna at 0,0,0, facing X coordinate.
+ * See http://www.antenna-theory.com/basics/friis.php
+ * Sensitivity is -85 dBm / -115 dB
+ *
+ * @param  tag_x       Tag x coord (m.) with respect to antenna
+ * @param  tag_y       Tag y coord (m.) with respect to antenna
+ * @param  freq        Transmission frequency (Hertzs)
+ * @param  txtPower    Transmitted power (dB)
+ * @return             Received power (dB)
+ */
+ double received_power_friis(double tag_x, double tag_y, double freq, double txtPower);
+ 
+ double received_power_friis(double tag_x, double tag_y, double freq, double txtPower, SplineFunction antennaGainsModel);
+
+/**
+ * Received signal estimated phase difference with pi ambiguity
+ * @param  tag_x       Tag x coord (m.) with respect to antenna
+ * @param  tag_y       Tag y coord (m.) with respect to antenna
+ * @param  freq        Transmission frequency (Hertzs)
+ * @return      phase difference (radians)
+ */
+double phaseDifference(double tag_x, double tag_y, double freq);
+
+
+/**
+ * Get tag detection boundaries for given configuration
+ * @param freq        RF signal frequency
+ * @param txtPower    Transmitted RF power (dB.)
+ * @param sensitivity Minimun power the reader can receive (dB.)
+ * @param distStep    distance step for internal power calculations
+ * @param minX        minimum X distance where rx power is over sensitivity
+ * @param minY        minimum Y distance where rx power is over sensitivity
+ * @param maxX        maximum X distance where rx power is over sensitivity
+ * @param maxY        maximum Y distance where rx power is over sensitivity
+ */
+void activeAreaFriis(double freq, double txtPower, double sensitivity, double distStep, double& minX, double& minY, double& maxX, double& maxY);
+
+
+void addMeasurement(double x, double y, double orientation, double rxPower, double phase, double freq, int i);
+
+std::string getPowLayerName(double freq_i);
+
+std::string getPhaseLayerName(double freq_i);
+
+std::string getTagLayerName(int tag_num);
+
+double received_power_friis_polar(double tag_r, double tag_h, double freq, double txtPower, SplineFunction antennaGainsModel);
+
+void getImageDebug(std::string layerName, std::string fileURI);
+Eigen::MatrixXf getPowProbCond(double rxPw, double f_i);
+Eigen::MatrixXf  getPhaseProbCond(double ph_i, double f_i);
+Eigen::MatrixXf  getProbCond(std::string layer_i, double x, double sig);
+
+void saveProbMapDebug(std::string savePATH, int tag_num, int step, double robot_x, double robot_y, double robot_head);
+
+
+void getImage(GridMap* gm,std::string layerName, std::string fileURI);
+
+void PrintRefMapWithTags(std::string fileURI);
+
+void PrintPowProb(std::string fileURI, double rxPw, double f_i);
+void PrintPhaseProb(std::string fileURI, double phi, double f_i);
+void PrintBothProb(std::string fileURI, double rxPw, double phi, double f_i);
+
+
+void PrintProb(std::string fileURI, Eigen::MatrixXf* prob_mat,  double sX, double sY, double res);
+void saveProbMaps(std::string savePath);
+
+void PrintProb(std::string fileURI, Eigen::MatrixXf* prob_mat);
+
+
+void debugInfo();
+
+}; // end class
+
+
+
+#endif
--- test_log_model.cpp
+++ test_log_model.cpp
@@ -1,1 +1,233 @@
-/home/manolofc/workspace/RFID/src/RFID/newModel/src/test_log_model2.cpp
+#include "RadarModel.hpp"
+
+
+#include <iostream>
+#include <vector>
+#include <boost/random.hpp>
+#include <boost/nondet_random.hpp>
+
+
+
+
+
+
+const int NARGS = 7;
+
+// quick build:
+// Create build folder and go there
+// cmake .. 
+// make
+//
+
+
+/*
+./mcdm/Images/mfc_test.pgm
+120 p x 200 p (gimp) == 200 p x 120 p (ric) 
+
+*/
+
+int main(int argc, char **argv)
+{
+
+  boost::random::random_device                  rand_dev;
+  boost::random::mt19937                        generator(rand_dev());
+
+
+  std::string mapFileURI = "/home/manolofc/workspace/mcdm/src/mcdm/Images/mfc_test.pgm";
+  double nx = 24;
+  double ny = 12;
+  double resolution = 0.1;
+  double sigma_power = 1;
+  double sigma_phase = 1;
+  double txtPower = -10;
+  int i = 0;
+  cout <<"You provided: (" << argc-1 << ") arguments"<<  std::endl;
+
+  if (argc > NARGS) {
+            mapFileURI = argv[++i];
+            nx = atoi(argv[++i]);
+            ny = atoi(argv[++i]);
+            resolution = atof(argv[++i]);
+            sigma_power = atof(argv[++i]);
+            sigma_phase = atof(argv[++i]);
+            txtPower = atof(argv[++i]);
+  } else {
+      cout <<"DEFAULTS! I will execute call equivalent to:" << std::endl;
+      cout << argv[0] << " " << mapFileURI  << " " << nx  << " " << ny  << " " << 
+              resolution  << " " << sigma_power  << " " << sigma_phase  << " " << txtPower <<  std::endl;
+  }
+  cout <<"PARAMETERS: " << std::endl;
+  // Radio model  ........................................................
+  cout <<"\tReference Map file: ["<< mapFileURI <<"]" << std::endl;
+  cout <<"\tRadar Active area: ("<< nx <<","<< ny <<") m." << std::endl;
+  cout <<"\tMap Resolution: ("<< resolution <<") m./cell" << std::endl;
+  cout <<"\tPower Noise std: ("<< sigma_power <<")" << std::endl;
+  cout <<"\tPhase Noise std: ("<< sigma_phase <<")" << std::endl;
+  cout <<"\tTransmitted power: ("<< txtPower <<") dB." << std::endl << std::endl;
+  
+  
+  // loading tag poses ........................................................
+  // Unit is meters. We multiply pixels by resolution to get them.
+  // 0,0 is at upper left of the map, with X increasing down and Y increasing right.
+
+  double absTag1_X = 37 * resolution;
+  double absTag1_Y = 58 * resolution;
+
+  double absTag2_X = 44 * resolution;
+  double absTag2_Y = 31 * resolution;
+  
+  double absTag3_X = 130 * resolution;
+  double absTag3_Y = 45 * resolution;
+  
+  double absTag4_X = 150 * resolution;
+  double absTag4_Y = 108 * resolution;
+
+  std::vector<std::pair<double,double>> tags_coord;
+  tags_coord.push_back(std::make_pair(absTag1_X, absTag1_Y));
+  tags_coord.push_back(std::make_pair(absTag2_X, absTag2_Y));
+  tags_coord.push_back(std::make_pair(absTag3_X, absTag3_Y));
+  tags_coord.push_back(std::make_pair(absTag4_X, absTag4_Y));
+  //  ........................................................
+  double f_i =  MIN_FREQ_NA; // 902e6 Hertzs
+
+  // create frequencies vector from possible ones:
+  
+  /*
+  Model with ALL freqs takes 1 sec to start. Too long for short tests
+  std::vector<double> freqs;
+  do{
+    freqs.push_back(f_i);
+    f_i+=STEP_FREQ_NA;
+  } while (f_i<=MAX_FREQ_NA);
+  */
+
+  std::vector<double> freqs{ MIN_FREQ_NA,MIN_FREQ_NA+STEP_FREQ_NA,MIN_FREQ_NA+2.0*STEP_FREQ_NA }; 
+
+
+  // later on, we will use this to get random freqs...
+  boost::random::uniform_int_distribution<>  distr(0, freqs.size()-1);
+
+  //  ........................................................
+
+
+  cout <<"Building radar model." << endl;
+  RadarModel rm(nx, ny, resolution, sigma_power, sigma_phase, txtPower, freqs, tags_coord, mapFileURI );
+  cout << "Radar model built." << endl;
+
+  // prints reference map with tags
+  rm.PrintRefMapWithTags("/tmp/scenario.png");      
+  //  - Map goes from 0,0 to (N,M)*resolution in meters
+  //  - Rcoods: up-left is 0,0. X increases down, and Y increases Right: swap x and y axes from images as they show on gimp 
+
+
+  // simple scenario
+  int NumReadings = 10;
+  // Unit is meters. We multiply pixels by resolution to get them.
+  double start_x = 40 * resolution;
+  double start_y = 55 * resolution;
+  double end_x = 150 * resolution;
+  double end_y = 75 * resolution;
+  
+  double robot_y;
+  double robot_x;
+  double robot_head0=atan2( end_y - start_y, end_x - start_x);
+  double robot_head;
+  double rxPower ;
+  double tag_x, delta_x;
+  double tag_y, delta_y;
+  double phase;
+
+  
+  // let's do 10 samples from start point and ing a straight line.
+  for (int i=0;i<NumReadings;i++){
+      // current robot position, map coordinates.
+      robot_y = start_y + ( (end_y - start_y) * ( i ) / (NumReadings - 1.0) );
+      robot_x = start_x + ( (end_x - start_x) * ( i ) / (NumReadings - 1.0) );
+      
+      for (int h=0;h<8;h++){
+        robot_head = robot_head0 + (2.0*M_PI*h/8);
+        std::cout<<"Robot at (" << robot_x << ", " << robot_y <<") m., (" << (robot_head*180.0/M_PI) << ") deg." << std::endl;
+        
+        //for each tag:
+        for (int t = 0; t < tags_coord.size(); t++){
+            // get relative robot-tag pose
+            // translate
+            delta_x = (tags_coord[t].first - robot_x ); 
+            delta_y = (tags_coord[t].second - robot_y);
+            // rotate
+            tag_x =  delta_x * cos(robot_head) + delta_y * sin(robot_head);
+            tag_y = -delta_x * sin(robot_head) + delta_y * cos(robot_head);
+
+            std::cout<<"\t- Tag [" << t << "] at position (" << tags_coord[t].first << ", " << tags_coord[t].second << ") m., rel position (" << tag_x << ", " << tag_y << ") m. " <<std::endl;
+
+            // get expected tag power with friis
+            f_i = freqs[distr(generator)]; 
+            rxPower = rm.received_power_friis( tag_x, tag_y, f_i, txtPower);
+
+            // get expected phase from tag
+            phase = rm.phaseDifference( tag_x,  tag_y,  f_i);
+            std::cout<<"\tReading at freq (" << f_i/1e6<< " MHz): (" << (rxPower+30) << ") dBm. ( " << phase << ") rads. " << std::endl << std::endl;
+
+            // add measurement to model
+            rm.addMeasurement(robot_x, robot_y, robot_head*180.0/M_PI,  rxPower,  phase,  f_i,  t);
+            //print maps
+            cout  << "Saving tag distribution maps... "<< endl;
+            int lineal_index = (8*(i+1))+(h+1);
+            rm.saveProbMapDebug("/tmp/",t,lineal_index,robot_x,robot_y, robot_head);
+        }
+        std::cout<<"Finished reading. " << std::endl << std::endl;
+    }
+  }
+  //print maps
+  cout  << "Saving tag distribution maps... "<< endl;
+  rm.saveProbMaps("/tmp/");
+  return 0;
+}
+
+
+
+/*
+
+  //rm.getImage("P_910.00", "/tmp/Average_power_map_f_910.png");            
+  //rm.getImage("D_910.00", "/tmp/Average_phase_map_f_910.png");  
+  //rm.PrintPowProb("/tmp/prob_rec_power_f_910.png", test_rxp, f_i);          
+  //rm.PrintPhaseProb("/tmp/prob_rec_phase_f_910.png", test_phase, f_i);          
+  
+  
+  // export tag locations into image, overimpressing background map.
+  // at least points should be in in white areas1=1 
+
+
+
+
+To read an arbitrary long tag loc list
+
+  double absTag_pose_x;
+  std::string absTag_string_x;
+  double absTag_pose_y;
+  std::string absTag_string_y;
+
+  // Create an output string stream
+  std::ostringstream streamObj3;
+
+  // Set Fixed -Point Notation
+  streamObj3 << std::fixed;
+
+  // Set precision to 2 digits
+  streamObj3 << std::setprecision(2);
+
+  std::vector<std::pair<double,double>> tags_coord;
+  for (int i=1;i<=numTags){
+    // flush streamObj3
+    streamObj3.clear();
+    //streamObj3.str("");
+    streamObj3 << numTags;
+
+    absTag_string_x = streamObj3.str();
+    absTag_string_y = streamObj3.str();
+    absTag_pose_x = config[absTag_string_x].as<double>();
+    absTag_pose_y = config[absTag_string_y].as<double>();
+    tags_coord.push_back(std::make_pair(absTag_pose_x, absTag_pose_y));
+  }
+
+*/
