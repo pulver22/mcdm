4cefc7708bf19eca86770c7de508a8215fe5e65f
Riccardo <pulver22@gmail.com>
2020-06-11 15:31:09 +0100

    Code released for publication

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,14 @@
 # INB_ENG
-./../build/mcdm_online_exploration ./../Images/inbeng_small.pgm 1 55 41 180 12 180 0.99 0 1 ./../config/tag_inbeng_2.yaml 902e6 0 0 0 0 1 0 /tmp/result_inbeng.csv /tmp/coverage_mcdm_inbeng.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
+./build/mcdm_online_exploration ./Images/inbeng_small.pgm 1 55 41 180 12 180 0.99 0 1 ./config/tag_inbeng_2.yaml 902e6 0 0 0 0 1 0 /tmp/result_inbeng.csv /tmp/coverage_mcdm_inbeng.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
 
 # INB_3123
-# ./../build/mcdm_online_exploration ./../Images/inb3123_small.pgm 1 105 78 180 6 180 0.99 0 1 ./../config/tag_inb3123_2.yaml 902e6 0 0 0 0 1 0 /tmp/result_inb3123_mcdm.csv /tmp/coverage_inb3123_mcdm.csv /tmp/distance_inb3123_mcdm.csv 1 /tmp/accuracy_inb3123_mcdm.csv 1
+# ./build/mcdm_online_exploration ./Images/inb3123_small.pgm 1 105 78 180 6 180 0.99 0 1 ./config/tag_inb3123_2.yaml 902e6 0 0 0 0 1 0 /tmp/result_inb3123_mcdm.csv /tmp/coverage_inb3123_mcdm.csv /tmp/distance_inb3123_mcdm.csv 1 /tmp/accuracy_inb3123_mcdm.csv 1
 
 #INB_ATRIUM
-# ./../build/mcdm_online_exploration ./../Images/inbatrium_small.pgm 1 127 99 180 9 180 0.99 0 1 ./../config/tag_inbatrium_2.yaml 902e6 0 0 0 0 0 1 /tmp/result_inbatrium.csv /tmp/coverage_mcdm_inbatrium.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
+# ./build/mcdm_online_exploration ./Images/inbatrium_small.pgm 1 127 99 180 9 180 0.99 0 1 ./config/tag_inbatrium_2.yaml 902e6 0 0 0 0 0 1 /tmp/result_inbatrium.csv /tmp/coverage_mcdm_inbatrium.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
 
 #NCFM
-# ./../build/mcdm_online_exploration ./../Images/ncfm_small.pgm 1 38 95 180 12 180 0.99 0 1 ./../config/tag_ncfm_0.yaml 902e6 0 1 0 0 0 0 /tmp/result_ncfm.csv /tmp/coverage_mcdm_ncfm.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
+# ./build/mcdm_online_exploration ./Images/ncfm_small.pgm 1 38 95 180 12 180 0.99 0 1 ./config/tag_ncfm_0.yaml 902e6 0 1 0 0 0 0 /tmp/result_ncfm.csv /tmp/coverage_mcdm_ncfm.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
 
 #OREBRO
-# ./../build/mcdm_online_exploration ./../Images/orebro_small.pgm 1 97 100 180 6 180 0.99 0 1 ./../config/tag_orebro_2.yaml 902e6 0 0 0 0 1 0 /tmp/result_orebro.csv /tmp/coverage_mcdm_orebro.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
+# ./build/mcdm_online_exploration ./Images/orebro_small.pgm 1 97 100 180 6 180 0.99 0 1 ./config/tag_orebro_2.yaml 902e6 0 0 0 0 1 0 /tmp/result_orebro.csv /tmp/coverage_mcdm_orebro.csv /tmp/distance_tag.csv 1 /tmp/accuracy.csv 1
